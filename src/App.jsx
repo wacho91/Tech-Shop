ead1ea7b94f2cfbf854d9a46081297d66e58bdae
wacho91 <criskol.71@gmail.com>
2024-08-21 11:31:57 -0500

    Services Component Complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import Category from "./components/Category/Category"
 import Category2 from "./components/Category/Category2"
 import Hero from "./components/Hero/Hero"
 import NavBar from "./components/NavBar/NavBar"
+import Services from "./components/Services/Services"
 
 function App() {
 
   return (
-    <div>
+    <div className="bg-white dark:bg-gray-900 dark:text-white duration-200 overflow-hidden">
       <NavBar />
       <Hero />
       <Category />
       <Category2 />
+      <Services />
     </div>
   )
 }
