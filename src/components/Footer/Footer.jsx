6bed2f373d76a32824bb3481975ac4d3659f6fae
wacho91 <criskol.71@gmail.com>
2024-08-23 11:30:05 -0500

    Footer complete

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,113 @@
+import { FaInstagram, FaLocationArrow, FaMobileAlt, FaTwitter } from "react-icons/fa"
+import { FaFacebook } from "react-icons/fa6"
 
+
+const FooterLinks = [
+    {
+        title: "Home",
+        link: "/#"
+    },
+
+    {
+        title: "About",
+        link: "/#about"
+    },
+
+    {
+        title: 'Blogs',
+        link: "/#blogs"
+    },
+
+    {
+        title: "Contact",
+        link: "/#contact"
+    }
+]
 
 const Footer = () => {
   return (
-    <div>
-      
+    <div className="dark:bg-gray-950">
+      <div className="container">
+        <div className="grid md:grid-cols-3 pb-20 pt-5">
+            {/*Company Details*/}
+            <div className="py-8 px-4">
+                <a 
+                    className="text-primary font-semibold tracking-widest text-2xl uppercase sm:text-3xl"
+                    href="#"
+                >
+                    TShop
+                </a>
+                <p className="text-gray-600 dark:text-white/70 lg:pr-24 pt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi est quae explicabo praesentium quod.</p>
+                <p className="text-gray-500 mt-4">Made Whit ❤️ by Criskol Design</p>
+            </div>
+            {/*Footer links*/}
+            <div className="col-span-2 grid grid-cols-2 md:grid-cols-3 md:pl-10">
+                <div className="py-8 px-4">
+                    <h1 className="text-xl font-bold sm:text-left mb-3">Important Links</h1>
+                    <ul className="space-y-3">
+                        {
+                            FooterLinks.map((data, index) => (
+                                <li key={index}>
+                                    <a
+                                        className="text-gray-600 dark:text-gray-400 hover:dark:text-white hover:text-black duration-300" 
+                                        href={data.link}
+                                    >
+                                        {data.title}
+                                    </a>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+                {/*----------*/}
+                <div className="py-8 px-4">
+                    <h1 className="text-xl font-bold sm:text-left mb-3">Quick Links</h1>
+                    <ul className="space-y-3">
+                        {
+                            FooterLinks.map((data, index) => (
+                                <li key={index}>
+                                    <a
+                                        className="text-gray-600 dark:text-gray-400 hover:dark:text-white hover:text-black duration-300" 
+                                        href={data.link}
+                                    >
+                                        {data.title}
+                                    </a>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+
+                {/*Company Address*/}
+                <div className="py-8 px-4 col-span-2 sm:col-auto">
+                    <h1 className="text-xl font-bold sm:text-left mb-3">Address</h1>
+                    <div>
+                        <div className="flex items-center gap-3">
+                            <FaLocationArrow />
+                            <p>Noida, Utar Pradesh</p>
+                        </div>
+                        <div className="flex items-center gap-3 mt-3">
+                            <FaMobileAlt />
+                            <p>+91 123456789</p>
+                        </div>
+
+                        {/*Social Links*/}
+                        <div className="flex items-center gap-3 mt-3">
+                            <a href="#">
+                                <FaInstagram className="text-3xl hover:text-primary duration-300"/>
+                            </a>
+                            <a href="#">
+                                <FaFacebook className="text-3xl hover:text-primary duration-300"/>
+                            </a>
+                            <a href="#">
+                                <FaTwitter className="text-3xl hover:text-primary duration-300"/>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
